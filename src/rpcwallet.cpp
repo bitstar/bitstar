4f6a1f639703cd1000c8dae5b555668371cec11b
Whit J <whitj00@users.noreply.github.com>
2015-01-29 16:53:29 -0800

    Fix Redeem Script issue

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1566,6 +1566,7 @@ public:
         int nRequired;
         ExtractDestinations(subscript, whichType, addresses, nRequired);
         obj.push_back(Pair("script", GetTxnOutputType(whichType)));
+        obj.push_back(Pair("hex", HexStr(subscript.begin(), subscript.end())));
         Array a;
         BOOST_FOREACH(const CTxDestination& addr, addresses)
             a.push_back(CBitcoinAddress(addr).ToString());
@@ -1574,6 +1575,7 @@ public:
             obj.push_back(Pair("sigsrequired", nRequired));
         return obj;
     }
+    
 };
 
 Value validateaddress(const Array& params, bool fHelp)
